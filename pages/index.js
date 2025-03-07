3a039a5349b179bfef89f94183e1a3223daf2273
ThalysRD <88295320+ThalysRD@users.noreply.github.com>
2025-03-07 01:49:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+
+function Home() {
+    return (<body>
+        <header>
+            <a>Serviços</a>
+            <a>Sobre mim</a>
+            <a>Agenda</a>
+            <a>Contato</a>
+        </header>
+        <main>
+            <h1>Luciana Pereira Dantas Pacheco Pacelli</h1>
+            <h2>Psicologia Infantil</h2>
+            <h3>CPR xx/xxxxx </h3>
+
+            <div>
+                <img />
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <a>Entre em contato</a>
+            </div>
+
+            <div>
+                <h1>Serviços</h1>
+                <div>
+                    <div>Psicoterapia
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        </p>
+                    </div>
+                    <div>Avaliação psicologica
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer><p>Luciana Pereira Dantas Pacheco Pacelli </p> <p>CRP XX/XXXXX</p> <p>(84)99999-9999</p> <p>lucianapereira@gmail.com</p> <a>Entre em contato</a></footer>
+    </body>)
+}
+
+export default Home;
